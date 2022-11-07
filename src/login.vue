0aeea1e40cc14240bf522b66906fb03cedaf0edc
客官 <3125667110@qq.com>
2022-11-07 10:06:24 +0800

    半个登陆

--- src/login.vue
+++ src/login.vue
@@ -1,7 +1,53 @@
 <script setup lang="ts">
 import { ref } from "vue";
+const form = ref({})
 </script>
 
 <template>
-  <div>登陆</div>
+  <div class="login">
+    <!-- <img class="loginBack" src=""> -->
+    <video
+      class="video"
+      :muted="true"
+      :loop="true"
+      :autoplay="true"
+      width="100%"
+      height="100%"
+      src="./assets/d51a96a0f6215e5a08e30ad11b35ff44_preview.mp4"
+    ></video>
+    <div class="loginBox">
+      <div class="login-title">登陆</div>
+      <div class="form"></div>
+    </div>
+  </div>
 </template>
+<style scoped lang="less">
+.login {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .video {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+  }
+  .loginBox {
+    position: relative;
+    z-index: 9999;
+    width: 400px;
+    height: 400px;
+    border-radius: 4px;
+    box-shadow: 0 2px 2px rgba(10, 16, 20, 0.24), 0 0 2px rgba(10, 16, 20, 0.12);
+    transition: box-shadow 0.5s;
+    .login-title {
+      width: 100%;
+      color: #333;
+      text-align: center;
+      padding: 10px 0px;
+    }
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import less from "less";
 import router from "./router/index";
 import "./style.css";
 import App from "./App.vue";
 import "element-plus/dist/index.css";
-import i18n from './i18n/'
+import i18n from "./i18n/";
 
 const pinia = createPinia(); // 创建Pinia实例
 const app = createApp(App);
 app.use(pinia);
 app.use(router);
-app.use(i18n)
+app.use(i18n);
+app.use(less as any);
 app.mount("#app");
