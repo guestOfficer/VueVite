cbee36f19744b3224011373a61d532ff9fb68588
郑建辉 <3125667110@qq.com>
2022-12-03 15:46:11 +0800

    less

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     preprocessorOptions: {
       less: {
         charset: false,
-        additionalData: '@import "./src/css/base.less";',
+        additionalData: '@import "./src/css/indes.less";',
       },
     },
   },
